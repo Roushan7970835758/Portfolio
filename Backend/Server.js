25df15b85b089a0412d0d537e277bd26f4414b02
Roushan7970835758 <rroushan40@gmail.com>
2024-11-23 23:39:16 +0530

    deployment1

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
+import buildPaht from "../"
 
 //dotevn configrueation
 dotenv.config();
@@ -11,6 +13,9 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//static files access
+app.use(express.static(path.join(__dirname,"../")))
 
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoutes"));
